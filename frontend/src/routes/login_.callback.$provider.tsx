660d13cf188aa97571f221f000d51e82aca33721
galst1234 <12615933+galst1234@users.noreply.github.com>
2026-06-12 00:32:17 +0300

    fix third party login?

--- frontend/src/routes/login_.callback.$provider.tsx
+++ frontend/src/routes/login_.callback.$provider.tsx
@@ -1,12 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { EmailPasswordPreBuiltUI } from "supertokens-auth-react/recipe/emailpassword/prebuiltui";
 import { ThirdPartyPreBuiltUI } from "supertokens-auth-react/recipe/thirdparty/prebuiltui";
-import { AuthPage } from "supertokens-auth-react/ui";
+import { getRoutingComponent } from "supertokens-auth-react/ui";
+
+const preBuiltUIList = [ThirdPartyPreBuiltUI, EmailPasswordPreBuiltUI];
 
 export const Route = createFileRoute("/login_/callback/$provider")({
   component: CallbackPage,
 });
 
 function CallbackPage() {
-  return <AuthPage preBuiltUIList={[ThirdPartyPreBuiltUI, EmailPasswordPreBuiltUI]} />;
+  return getRoutingComponent(preBuiltUIList);
 }
