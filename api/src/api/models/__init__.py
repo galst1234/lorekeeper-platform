c55558a3c722d24a84ec122f4621e1c615abb044
galst1234 <12615933+galst1234@users.noreply.github.com>
2026-07-02 19:29:51 +0300

    feat(api): add Item model and items table migration

--- api/src/api/models/__init__.py
+++ api/src/api/models/__init__.py
@@ -1,6 +1,7 @@
 from api.models.campaign import Campaign
 from api.models.character import Character, CharacterType
+from api.models.item import Item
 from api.models.membership import CampaignMember, MemberRole
 from api.models.user import User, UserAuthMethod
 
-__all__ = ["Campaign", "CampaignMember", "Character", "CharacterType", "MemberRole", "User", "UserAuthMethod"]
+__all__ = ["Campaign", "CampaignMember", "Character", "CharacterType", "Item", "MemberRole", "User", "UserAuthMethod"]
