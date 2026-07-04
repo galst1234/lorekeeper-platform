1eeecfa7064894cccb4cccc738284dbf7819b112
galst1234 <12615933+galst1234@users.noreply.github.com>
2026-07-04 14:07:14 +0300

    feat: add chronicle entries API router

--- api/src/api/routers/campaigns/router.py
+++ api/src/api/routers/campaigns/router.py
@@ -1,8 +1,9 @@
-from api.routers.campaigns import characters, invites, items, members
+from api.routers.campaigns import characters, chronicle, invites, items, members
 from api.routers.campaigns.campaigns import detail_router, router
 
 detail_router.include_router(invites.router)
 detail_router.include_router(characters.router)
 detail_router.include_router(items.router)
+detail_router.include_router(chronicle.router)
 detail_router.include_router(members.router)
 router.include_router(detail_router)
